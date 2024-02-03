ed6f437098b5e8168f8a1e402d4014f8c0524e5a
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-02-03 20:13:56 +0530

    🗄️ | added Book, Review models

--- src/models.rs
+++ src/models.rs
@@ -9,6 +9,24 @@ pub struct User {
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Book {
+    pub title: String,
+    pub author: String,
+    pub genre: String,
+    description: String,
+    avg_rating: u8,
+    pub reviews: Vec<Review>
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Review {
+    pub user: String,
+    title: String,
+    content: String,
+    rating: u8
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct Record {
     #[allow(dead_code)]
     id: Thing,
